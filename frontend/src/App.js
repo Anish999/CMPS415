82a9bf410e1383a8c259cafd0d7648b1a6c66a53
Anish Manandhar <anish.manandhar@selu.edu>
2019-05-20 13:25:43 -0500

    Basic Navigation Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render(){
+    return (
       <BrowserRouter>
-      <Switch>
-          <Redirect from="/" to="/auth" exact/>
-          <Route path="/auth" component = {AuthPage} />
-          <Route path="/events" component = {EventsPage} />
-          <Route path="/bookings" component = {BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main>
+            <Switch>
+              <Redirect from="/" to="/auth" exact/>
+              <Route path="/auth" component = {AuthPage} />
+              <Route path="/events" component = {EventsPage} />
+              <Route path="/bookings" component = {BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
